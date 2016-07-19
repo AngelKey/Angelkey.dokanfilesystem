40d4a702300b28d105a607263b4b7f599446a1ca
Marinko Babic <marinko.babic@gmx.net>
2016-07-20 01:02:07 +0200

    return FileFsSizeInformation if there are not Events waiting. This way antivirus software get the information back and the do not block the incoming event waiters

--- sys/volume.c
+++ sys/volume.c
@@ -66,7 +66,7 @@ DokanDispatchQueryVolumeInformation(__in PDEVICE_OBJECT DeviceObject,
         break;
       }
 
-      DDbgPrint("  Still no threads for processing available\n");
+      DDbgPrint("  Still no threads for processing available - FileFsVolumeInformation \n");
       PFILE_FS_VOLUME_INFORMATION FsVolInfo;
 
       if (irpSp->Parameters.QueryVolume.Length <
@@ -105,6 +105,36 @@ DokanDispatchQueryVolumeInformation(__in PDEVICE_OBJECT DeviceObject,
 
     case FileFsSizeInformation:
       DDbgPrint("  FileFsSizeInformation\n");
+      if (vcb->HasEventWait) {
+          break;
+      }
+
+      DDbgPrint("  Still no threads for processing available - FileFsSizeInformation \n");
+      PFILE_FS_SIZE_INFORMATION sizeInfo;
+
+      if (irpSp->Parameters.QueryVolume.Length <
+          sizeof(FILE_FS_SIZE_INFORMATION)) {
+          status = STATUS_BUFFER_OVERFLOW;
+          __leave;
+      }
+      
+      ULONGLONG freeBytesAvailable = 512 * 1024 * 1024;
+      ULONGLONG totalBytes = 1024 * 1024 * 1024;
+      ULONGLONG freeBytes = 512 * 1024 * 1024;
+
+      sizeInfo = (PFILE_FS_SIZE_INFORMATION)buffer;
+      sizeInfo->TotalAllocationUnits.QuadPart =
+          totalBytes / DOKAN_DEFAULT_ALLOCATION_UNIT_SIZE;
+      sizeInfo->AvailableAllocationUnits.QuadPart =
+          freeBytesAvailable / DOKAN_DEFAULT_ALLOCATION_UNIT_SIZE;
+      sizeInfo->SectorsPerAllocationUnit =
+          DOKAN_DEFAULT_ALLOCATION_UNIT_SIZE / DOKAN_DEFAULT_SECTOR_SIZE;
+      sizeInfo->BytesPerSector = DOKAN_DEFAULT_SECTOR_SIZE;
+
+      Irp->IoStatus.Information = sizeof(FILE_FS_SIZE_INFORMATION);
+      status = STATUS_SUCCESS;
+      __leave;
+
       break;
 
     case FileFsDeviceInformation: {
