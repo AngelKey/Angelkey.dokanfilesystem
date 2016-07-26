2988c77d17dec44dbc47fe6fcb6555dbbabc1d66
Steve Sanders <steve@keyba.se>
2016-07-26 13:08:36 -0700

    Separate wix build.bat

--- dokan_wix/build.bat
+++ dokan_wix/build.bat
@@ -3,17 +3,20 @@ set VCTargetsPath=%PROGRAMFILES%
 IF %processor_architecture%==AMD64 set VCTargetsPath=%PROGRAMFILES(x86)%
 set VCTargetsPath=%VCTargetsPath%\MSBuild\Microsoft.Cpp\v4.0\V140
 
-REM set version info, edit version.txt before running the batch
-if NOT exist SetAssemblyVersion\bin\Release\SetAssemblyVersion.exe (
-	msbuild SetAssemblyVersion.sln /p:Configuration=Release /p:Platform="Any CPU" /t:rebuild 
+:: Jenkins tends to barf when this build.bat calls the other one, so do it separately
+if NOT %1. == WIXONLY. ( 
+	REM set version info, edit version.txt before running the batch
+	if NOT exist SetAssemblyVersion\bin\Release\SetAssemblyVersion.exe (
+		msbuild SetAssemblyVersion.sln /p:Configuration=Release /p:Platform="Any CPU" /t:rebuild 
+	)
+	SetAssemblyVersion\bin\Release\SetAssemblyVersion ..\CHANGELOG.md version.xml ..\
+
+	cd ..
+	call .\build.bat
+	Powershell.exe -executionpolicy remotesigned -File sign.ps1
+
+	cd dokan_wix
 )
-SetAssemblyVersion\bin\Release\SetAssemblyVersion ..\CHANGELOG.md version.xml ..\
-
-cd ..
-call .\build.bat
-Powershell.exe -executionpolicy remotesigned -File sign.ps1
-
-cd dokan_wix
 
 MakeCab /f dokanx64.ddf
 MakeCab /f dokanx86.ddf
