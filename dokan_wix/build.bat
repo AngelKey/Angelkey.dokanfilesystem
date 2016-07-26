5f45d35b07e883e34e1ed79daba991a85ed02c0d
Steve Sanders <steve@keyba.se>
2016-07-26 13:08:36 -0700

    Separate wix build.bat

--- dokan_wix/build.bat
+++ dokan_wix/build.bat
@@ -3,16 +3,19 @@ set VCTargetsPath=%PROGRAMFILES%
 IF %processor_architecture%==AMD64 set VCTargetsPath=%PROGRAMFILES(x86)%
 set VCTargetsPath=%VCTargetsPath%\MSBuild\Microsoft.Cpp\v4.0\V140
 
-REM set version info, edit version.txt before running the batch
-if NOT exist SetAssemblyVersion\bin\Release\SetAssemblyVersion.exe (
-	msbuild SetAssemblyVersion.sln /p:Configuration=Release /p:Platform="Any CPU" /t:rebuild 
-)
-SetAssemblyVersion\bin\Release\SetAssemblyVersion version.txt /setversion version.xml ..\
+:: Jenkins tends to barf when this bud.bat calls the other one, so do it separately
+if NOT %1. == WIXONLY. ( 
+	REM set version info, edit version.txt before running the batch
+	if NOT exist SetAssemblyVersion\bin\Release\SetAssemblyVersion.exe (
+		msbuild SetAssemblyVersion.sln /p:Configuration=Release /p:Platform="Any CPU" /t:rebuild 
+	)
+	SetAssemblyVersion\bin\Release\SetAssemblyVersion version.txt /setversion version.xml ..\
 
-cd ..
-.\build.bat
-Powershell.exe -executionpolicy remotesigned -File sign.ps1
-cd dokan_wix
+	cd ..
+	.\build.bat
+	Powershell.exe -executionpolicy remotesigned -File sign.ps1
+	cd dokan_wix
+)
 
 IF EXIST C:\cygwin ( powershell -Command "(gc version.xml) -replace 'BuildCygwin=\"false\"', 'BuildCygwin=\"true\"' | sc version.xml" ) ELSE ( powershell -Command "(gc version.xml) -replace 'BuildCygwin=\"true\"', 'BuildCygwin=\"false\"' | sc version.xml" )
 
